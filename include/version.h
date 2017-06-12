9977a0322da4c664e9a192f47fa7cc9c1eaf6e61
lesaff-r <remi.lesaffre@epitech.eu>
2017-06-12 15:49:19 +0200

    Added source/include groups support for Visual Studio and Project version file

---
+++ include/version.h
@@ -0,0 +1,25 @@
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2017 Lesaffre Remi (remi.lesaffre@gmail.com)
+//
+
+#pragma once
+
+#define ENGINE_VERSION_MAJOR 0
+#define ENGINE_VERSION_MINOR 1
+#define ENGINE_VERSION_PATCH 1
+#define ENGINE_VERSION_EXTRA 0
+#define ENGINE_VERSION       "0.1.1"
+#define ENGINE_VERSION_FULL  "0.1.1.0"
